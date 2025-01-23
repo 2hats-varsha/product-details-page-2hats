0b98e984b43065909a80d71628089ef53e3f9309
2hats-varsha <varshac@2hatslogic.com>
2025-01-23 15:34:46 +0530

    add size chart modal

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,20 @@ radioButton.forEach((radio) => {
         selectedColorSpan.textContent = event.target.value;
     });
   });
+
+  // Select elements
+const sizeChartButton = document.getElementById("sizeChartButton");
+const modal = document.getElementById("popup");
+const closeModalButton = document.getElementById("closeModalButton");
+sizeChartButton.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+closeModalButton.addEventListener("click", () => {
+    debugger
+  modal.style.display = "none";
+});
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
