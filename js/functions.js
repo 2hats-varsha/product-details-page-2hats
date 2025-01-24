3a2812fe306b23f9eab018b0079e888e78503a67
2hats-varsha <varshac@2hatslogic.com>
2025-01-24 11:53:07 +0530

    slider - starts

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,52 @@ searchInput.select();
     }})
     
    
+// slider
+const images = ["./assets/images/product.webp","./assets/images/imagepart2.webp","./assets/images/imagepart3.webp","./assets/images/imagepart4.webp","./assets/images/imagepart2.jpg"];
+
+const mainImage = document.getElementById('main-image');
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+let currentIndex = 0;
+const updateGallery = (index) => {
+    currentIndex = index;
+    mainImage.src = images[currentIndex];
+    thumbnails.forEach((image, i) => {
+        image.classList.toggle('selected', i === currentIndex);
+        image.addEventListener('mouseover', () => {
+            image.classList.add('overlay')
+        })
+        image.addEventListener('mouseout', () => {
+            image.classList.remove('overlay')
+        })
+    })
+}
+const changethumbnailImage= (index)=>{
+    if(index === images.length){
+        index = images.length -1;
+    }
+    updateGallery(index)
+}
+thumbnails.forEach((image,i)=>{
+    image.addEventListener('click',()=>{
+        updateGallery(i)
+    })
+})
+thumbnails[thumbnails.length - 1].addEventListener('click', () => {
+    updateGallery(images.length - 1)
+})
+const nextButton = ()=>{
+    nextIndex = currentIndex +1;
+    if(nextIndex >= images.length){
+        nextIndex=0;
+    }
+    updateGallery(nextIndex);
+}
+const prevButton = ()=>{
+    debugger
+    prevIndex = currentIndex -1;
+    if(prevIndex < 0){
+        prevIndex =0;
+    }
+    updateGallery(prevIndex);
+}
