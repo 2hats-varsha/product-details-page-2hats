1e01a75b935455c19a2ddb9971c45ec0a2002561
2hats-varsha <varshac@2hatslogic.com>
2025-01-24 13:11:33 +0530

    scroll completed

--- js/functions.js
+++ js/functions.js
@@ -31,10 +31,13 @@ searchInput.select();
     
    
 // slider
-const images = ["./assets/images/product.webp","./assets/images/imagepart2.webp","./assets/images/imagepart3.webp","./assets/images/imagepart4.webp","./assets/images/imagepart2.jpg"];
+const images = ["./assets/images/product.webp","./assets/images/imagepart2.webp","./assets/images/imagepart3.webp","./assets/images/imagepart4.webp","./assets/images/imagepart2.jpg","./assets/images/product.webp","./assets/images/imagepart2.webp","./assets/images/imagepart3.webp","./assets/images/imagepart4.webp","./assets/images/imagepart2.jpg"];
 
 const mainImage = document.getElementById('main-image');
 const thumbnails = document.querySelectorAll('.thumbnail');
+const thumbnailsContainer = document.querySelector('.thumbnails-container');
+const prevArrow = document.querySelector('.left');
+const nextArrow = document.querySelector('.right');
 
 let currentIndex = 0;
 const updateGallery = (index) => {
@@ -57,25 +60,45 @@ const changethumbnailImage= (index)=>{
     updateGallery(index)
 }
 thumbnails.forEach((image,i)=>{
-    image.addEventListener('click',()=>{
-        updateGallery(i)
+    debugger
+    image.addEventListener('click',(e)=>{
+        e.preventDefault();
+        updateGallery(i);
+       
     })
 })
 thumbnails[thumbnails.length - 1].addEventListener('click', () => {
     updateGallery(images.length - 1)
 })
-const nextButton = ()=>{
-    nextIndex = currentIndex +1;
-    if(nextIndex >= images.length){
-        nextIndex=0;
+
+nextArrow.addEventListener('click', (e) => {
+    e.preventDefault();
+    nextIndex = currentIndex + 1;
+    if (nextIndex >= images.length) {
+        nextIndex = 0;
     }
     updateGallery(nextIndex);
-}
-const prevButton = ()=>{
+    thumbnailsContainer.scrollTo({ left: nextIndex * thumbnails[0].offsetWidth, behavior: 'smooth' })
+})
+
+prevArrow.addEventListener('click', (e) => {
     debugger
-    prevIndex = currentIndex -1;
+    e.preventDefault();
+   let  prevIndex = currentIndex - 1;
     if(prevIndex < 0){
-        prevIndex =0;
+        prevIndex = images.length - 1;
     }
     updateGallery(prevIndex);
-}
+    thumbnailsContainer.scrollTo({left:prevIndex * thumbnails[0].offsetWidth, behavior: 'smooth'})
+
+})
+thumbnails.forEach((image, i) => {
+    image.addEventListener('click', (e) => {
+        e.preventDefault();
+        updateGallery(i);
+        thumbnailsContainer.scrollTo({
+            left: i * thumbnails[0].offsetWidth, 
+            behavior: 'smooth'
+        });
+    });
+});
